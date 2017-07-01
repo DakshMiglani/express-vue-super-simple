3fa6a7e227cb35285e76ff6a039b40552d666c04
Daksh Miglani <hello@dak.sh>
2017-07-01 16:34:52 +0530

    Added a Comment, for ease of use.

--- views/layout.vue
+++ views/layout.vue
@@ -4,7 +4,9 @@
         <head>
             <meta charset="UTF-8">
             <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+            <!-- For Production Use - https://vuejs.org/js/vue.min.js -->
             <script src="https://unpkg.com/vue/dist/vue.js"></script>
+            <!-- For Production Use - https://vuejs.org/js/vue.min.js -->
         </head>
         <body>
             {{{app}}}
